a2e5fb7467ca67e7f68b94bb67b3ed40eafab6d9
2Neo2 <64828077+2Neo2@users.noreply.github.com>
2021-05-31 23:04:27 +0300

    Update AppConstance.cs

--- Constance/AppConstance.cs
+++ Constance/AppConstance.cs
@@ -3,6 +3,6 @@ namespace RoBuilt.Constance
 {
     public class AppConstance
     {
-        public const string GoogleMapsApi = "AIzaSyCUX81OWveuxEEVGzUKfjEr4ZiQwQh_50E";
+        public const string GoogleMapsApi = "";
     }
 }
